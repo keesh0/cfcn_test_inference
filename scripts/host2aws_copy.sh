46ccb373c8d66603d9dbd2164f3f0ddc36aaa3ce
Eric Kischell <kischell.eric@mayo.edu>
2019-05-09 19:53:08 -0500

    removed bad olde file with spaces

--- scripts/host2aws_copy.sh
+++ scripts/host2aws_copy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#copy files to AWS EC2 instance
+#copy files to an AWS EC2 instance
 #should be run from cfcn script dirs
 
 SCRIPT_NAME="$(basename ${0})"
@@ -30,7 +30,7 @@ host2aws_copy.sh usage: $0 options
 
 OPTIONS:
     -h            [optional]  help, Show this message
-    -i <path>     [required]  AWS EC2 instance prefix  i.e. ec2-34-207-189-19
+    -a <path>     [required]  AWS EC2 instance prefix  i.e. ec2-34-207-189-19
 \n
 DOCS
 
@@ -43,20 +43,24 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-while getopts ":hi:" OPTION
+AWS_EC2_PREFIX="not set"
+while getopts ":ha:" OPTION
 do
     case $OPTION in
-        h) logInfo "${DOCS}"; exit 0 ;;
-        i) AWS_EC2_PREFIX="$(readlink -m ${OPTARG})" ;;
-        \?) logInfo "Invalid option: ${OPTARG}"; exit 1 ;;
-        :) logInfo "Missing argument for ${OPTARG}"; exit 1 ;;
-        *) logInfo "Unexpected option ${OPTARG}"; exit 1 ;;
+        h) echo "${DOCS}"; exit 0 ;;
+        a) AWS_EC2_PREFIX="${OPTARG}" ;;
+        \?) echo "Invalid option: ${OPTARG}"; exit 1 ;;
+        :) echo "Missing argument for ${OPTARG}"; exit 1 ;;
+        *) echo "Unexpected option ${OPTARG}"; exit 1 ;;
     esac
 done
 
 #Mac to AWS
 #copy over fresh python script
 scp -i "~/.ssh/eric-key.pem" cascaded_unet_inference.py ubuntu@${AWS_EC2_PREFIX}.compute-1.amazonaws.com:~/cascaded_unet_inference.py  #from mac to AWS
+scp -i "~/.ssh/eric-key.pem" test_cascaded_unet_inference.py ubuntu@${AWS_EC2_PREFIX}.compute-1.amazonaws.com:~/test_cascaded_unet_inference.py  #from mac to AWS
+scp -i "~/.ssh/eric-key.pem" aws2docker.sh ubuntu@${AWS_EC2_PREFIX}.compute-1.amazonaws.com:~/aws2docker.sh  #from mac to AWS
+scp -i "~/.ssh/eric-key.pem" copy_results_from_docker_to_host.sh ubuntu@${AWS_EC2_PREFIX}.compute-1.amazonaws.com:~/copy_results_from_docker_to_host.sh  #from mac to AWS
 
 #copy over original and CPU-forced models
 scp -i "~/.ssh/eric-key.pem" ../models/cascadedfcn/step1/step1_deploy.prototxt.orig ubuntu@${AWS_EC2_PREFIX}.compute-1.amazonaws.com:~/step1_deploy.prototxt.orig
